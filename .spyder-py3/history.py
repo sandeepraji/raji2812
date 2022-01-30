a3753163d613dfcc561f5a1cf9ceaacf58ca6c60
sandeepraj <91003537+sandeepraji@users.noreply.github.com>
2022-01-30 17:53:30 +0530

    initial commit

---
+++ .spyder-py3/history.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Jan 30 14:06:46 2022)---
+runfile('C:/Users/sandeep_n/.spyder-py3/temp.py', wdir='C:/Users/sandeep_n/.spyder-py3')
+runcell(0, 'C:/Users/sandeep_n/.spyder-py3/temp.py')
+runfile('C:/Users/sandeep_n/.spyder-py3/temp.py', wdir='C:/Users/sandeep_n/.spyder-py3')
+runcell(0, 'C:/Users/sandeep_n/.spyder-py3/untitled0.py')
+runfile('C:/Users/sandeep_n/.spyder-py3/Diabetes Prediction Web Application.py', wdir='C:/Users/sandeep_n/.spyder-py3')
